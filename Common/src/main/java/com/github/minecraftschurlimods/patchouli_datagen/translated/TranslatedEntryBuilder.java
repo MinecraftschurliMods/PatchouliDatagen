8e8f6a50eca85dffde284cac7fdb1ee05c8fc056
Minecraftschurli <minecraftschurli@gmail.com>
2022-05-27 12:19:46 +0200

    make TranslatedEntryBuilder#getLangKey public

--- Common/src/main/java/com/github/minecraftschurlimods/patchouli_datagen/translated/TranslatedEntryBuilder.java
+++ Common/src/main/java/com/github/minecraftschurlimods/patchouli_datagen/translated/TranslatedEntryBuilder.java
@@ -15,58 +15,58 @@ public class TranslatedEntryBuilder extends EntryBuilder<TranslatedBookBuilder, 
 
     @Override
     public TranslatedEntryBuilder addSimpleImagePage(ResourceLocation image, String text, String title) {
-        String key = getLangKey();
+        String key = getLangKey(pageCount());
         return super.addSimpleImagePage(image, putLangKey(key+".text", text), putLangKey(key+".title", title));
     }
 
     @Override
     public RecipePageBuilder addRecipePage(String type, ResourceLocation recipe, String text, String title) {
-        String key = getLangKey();
+        String key = getLangKey(pageCount());
         return super.addRecipePage(type, recipe, putLangKey(key+".text", text), putLangKey(key+".title", title));
     }
 
     @Override
     public TranslatedEntryBuilder addSimpleSpotlightPage(ItemStack stack, String text, String title) {
-        String key = getLangKey();
+        String key = getLangKey(pageCount());
         return super.addSimpleSpotlightPage(stack, putLangKey(key+".text", text), putLangKey(key+".title", title));
     }
 
     @Override
     public TranslatedEntryBuilder addSimpleLinkPage(String url, String linkText, String title, String text) {
-        String key = getLangKey();
+        String key = getLangKey(pageCount());
         return super.addSimpleLinkPage(url, putLangKey(key+".linkText", linkText), putLangKey(key+".title", title), putLangKey(key+".text", text));
     }
 
     @Override
     public LinkPageBuilder addLinkPage(String url, String linkText) {
-        String key = getLangKey();
+        String key = getLangKey(pageCount());
         return super.addLinkPage(url, putLangKey(key+".linkText", linkText));
     }
 
     @Override
     public TranslatedEntryBuilder addSimpleMultiblockPage(String name, String text, ResourceLocation multiblock) {
-        String key = getLangKey();
+        String key = getLangKey(pageCount());
         return super.addSimpleMultiblockPage(name, putLangKey(key+".text", text), multiblock);
     }
 
     @Override
     public MultiblockPageBuilder addMultiblockPage(String name, ResourceLocation multiblock) {
-        String key = getLangKey();
+        String key = getLangKey(pageCount());
         return super.addMultiblockPage(putLangKey(key+".title", name), multiblock);
     }
 
     @Override
     public TextPageBuilder addTextPage(String text, String title) {
-        String key = getLangKey();
+        String key = getLangKey(pageCount());
         return super.addTextPage(putLangKey(key+".text", text), putLangKey(key+".title", title));
     }
 
-    private String getLangKey() {
+    public String getLangKey(int page) {
         return "item.%s.%s.%s.%s".formatted(
                 parent.getBookId().getNamespace(),
                 parent.getBookId().getPath(),
                 getId().getPath().replaceAll("/", "."),
-                "page" + pageCount());
+                "page" + page);
     }
 
     protected String putLangKey(String key, String text) {
